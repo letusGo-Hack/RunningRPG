64554e8e3a5901620888f11c0e39ca07b8597e9b
EE-Yat-Ho <enough6157@naver.com>
2023-06-10 14:25:30 +0900

    add Utils, CoreMotionService

---
+++ RunningRPG/Utils/CoreMotionService.swift
@@ -0,0 +1,18 @@
+//
+//  CoreMotionService.swift
+//  RunningRPG
+//
+//  Created by ParkYoungHo on 2023/06/10.
+//
+
+import SwiftUI
+
+struct CoreMotionService: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreMotionService()
+}
