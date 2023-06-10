2d7ba18f257e6c8f13c39b5f2b7ec0a01eec238b
min <75626237+mingging@users.noreply.github.com>
2023-06-10 14:22:20 +0900

    Conflict File 교체

---
+++ RunningRPG/DynamicIslandView/Avatar.swift
@@ -0,0 +1,16 @@
+//
+//  Avatar.swift
+//  RunningRPG
+//
+//  Created by min on 2023/06/10.
+//
+
+import SwiftUI
+
+struct Avatar: View {
+    var body: some View {
+        ZStack {
+            Image("human_small")
+        }
+    }
+}
---
+++ RunningRPGDynamicIsland/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  RunningRPGDynamicIsland
+//
+//  Created by min on 2023/06/10.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
