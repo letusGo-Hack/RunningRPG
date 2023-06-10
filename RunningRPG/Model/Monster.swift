9630ec44cff01157405d41fa3db4d7ce51856bf9
EE-Yat-Ho <enough6157@naver.com>
2023-06-10 16:55:26 +0900

    merge error solve

--- RunningRPG/Model/Monster.swift
+++ RunningRPG/Model/Monster.swift
@@ -23,27 +23,29 @@ struct Monster: Hashable, Codable, Identifiable {
 }
 
 enum MonsterType {
-  case swordShield
-  case pizza
-  case gun
-  case squirrel
-
-  var urls: String {
-    switch self {
-    case .squirrel:
-      return "monster_ squirrel"
-    case .gun:
-      return "monster_gun"
-    case .pizza:
-      return "monster_pizza"
-    case .swordShield:
-      return "monster_swordshield"
+    case swordShield
+    case pizza
+    case gun
+    case squirrel
+    
+    var urls: String {
+        switch self {
+        case .squirrel:
+            return "monster_ squirrel"
+        case .gun:
+            return "monster_gun"
+        case .pizza:
+            return "monster_pizza"
+        case .swordShield:
+            return "monster_swordshield"
+        }
     }
-  }
+}
+
 extension Monster {
     static func generatMock() -> [Monster] {
         return [
-            Monster(name: "칼든사람", hp: 100, currentHp: 100, avatar: "monster_swordshield_small")
+            Monster(name: "칼든사람", hp: 100, currentHp: 100, avatar: "monster_swordshield_small", level: 0, isOpen: true)
         ]
         
         
