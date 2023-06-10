d83c39c522b0c0d9f6608035b164a2a4565d0cc2
dragerkk <dragerkk@gmail.com>
2023-06-10 13:52:45 +0900

    HomeBottomView UI fin

--- RunningRPG/Tabbar/Home/HomeBottomView.swift
+++ RunningRPG/Tabbar/Home/HomeBottomView.swift
@@ -9,23 +9,42 @@ import SwiftUI
 
 struct HomeBottomView: View {
     var body: some View {
-		HStack {
-			VStack { //left side equiptment
-				Image(systemName: "person")
-				Image(systemName: "person")
-				Image(systemName: "person")
+		VStack {
+//			HStack {
+//				//Damage View
+//				Text("Damage: ")
+//
+//				Text("test")
+//			}
+			Text("My Damage")
+				.padding(.bottom, 30)
+				.font(.system(size: 22))
+				.bold()
+				.foregroundColor(.red)
+			//Equipments & Character View
+			HStack {
+				VStack { //left side equiptment
+					Image(systemName: "person")
+					Image(systemName: "person")
+						.padding(.top, 10)
+					Image(systemName: "person")
+						.padding(.top, 10)
+				}
+				
+				Image("human") //center
+					.padding(.leading, 30)
+					.padding(.trailing, 30)
+				
+				VStack { //right side equiptment
+					Image(systemName: "person.fill")
+					Image(systemName: "person.fill")
+						.padding(.top, 10)
+					Image(systemName: "person.fill")
+						.padding(.top, 10)
+				}
 			}
-			HStack { //center
-				Image(systemName: "cloud.fill")
-			}
-			
-			VStack { //right side equiptment
-				Image(systemName: "person.fill")
-				Image(systemName: "person.fill")
-				Image(systemName: "person.fill")
-			}
-			
 		}
+		
     }
 }
 
