337010d70a70273d82b42d33184d36b3ece0cdd6
psychehose <psychehose@gmail.com>
2023-06-10 15:38:42 +0900

    [#2] Dynamic 누락 파일 생성

---
+++ RunningRPGDynamicIsland/RunningRPGDynamicIslandBundle.swift
@@ -0,0 +1,17 @@
+//
+//  RunningRPGDynamicIslandBundle.swift
+//  RunningRPGDynamicIsland
+//
+//  Created by min on 2023/06/10.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RunningRPGDynamicIslandBundle: WidgetBundle {
+    var body: some Widget {
+        RunningRPGDynamicIsland()
+        RunningRPGDynamicIslandLiveActivity()
+    }
+}
