013d082467844e9e479a9c613517460e75fb78cc
dragerkk <dragerkk@gmail.com>
2023-06-10 13:11:58 +0900

    add HomeBottomView

---
+++ RunningRPG/Tabbar/Home/HomeBottomView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeBottomView.swift
+//  RunningRPG
+//
+//  Created by 강주원 on 2023/06/10.
+//
+
+import SwiftUI
+
+struct HomeBottomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeBottomView()
+}
