5a8ecd49f476ebd68091be18418bd501d413bc9c
EE-Yat-Ho <enough6157@naver.com>
2023-06-10 14:21:14 +0900

    CoreMotion Test

--- RunningRPG/Tabbar/Home/HomeTopView.swift
+++ RunningRPG/Tabbar/Home/HomeTopView.swift
@@ -5,9 +5,21 @@
 //  Created by ParkYoungHo on 2023/06/10.
 //
 
+
+
 import SwiftUI
+import CoreMotion
 
 struct HomeTopView: View {
+    
+    let motionManager = CMMotionManager()
+    let queue = OperationQueue()
+
+    @State private var pitch = Double.zero
+    @State private var yaw = Double.zero
+    @State private var roll = Double.zero
+    
+    
     var body: some View {
         
         ZStack {
@@ -20,10 +32,21 @@ struct HomeTopView: View {
                     .resizable()
                     .scaledToFit()
                 
-                Rectangle()
-                    .fill(Color.clear)
-                    .frame(width: 150, height: 150)
-                
+                VStack {
+                    
+                    Text("걸음수 : \(pitch)")
+                        .background(Color.white)
+                    
+                    Text("걸음수 : \(yaw)")
+                        .background(Color.white)
+                    
+                    Text("걸음수 : \(roll)")
+                        .background(Color.white)
+                    
+                    Rectangle()
+                        .fill(Color.clear)
+                        .frame(width: 150, height: 150)
+                }
                 VStack {
                     
                     HStack(spacing: 0) {
@@ -44,6 +67,24 @@ struct HomeTopView: View {
                     
                 }
                 
+            }
+        }.onAppear {
+            self.motionManager.startDeviceMotionUpdates(to: self.queue) { (data: CMDeviceMotion?, error: Error?) in
+                guard let data = data else {
+                    print("Error: \(error!)")
+                    return
+                }
+                let attitude: CMAttitude = data.attitude
+                
+                print("pitch: \(attitude.pitch)")
+                print("yaw: \(attitude.yaw)")
+                print("roll: \(attitude.roll)")
+                
+                DispatchQueue.main.async {
+                    self.pitch = attitude.pitch
+                    self.yaw = attitude.yaw
+                    self.roll = attitude.roll
+                }
             }
         }
     }
